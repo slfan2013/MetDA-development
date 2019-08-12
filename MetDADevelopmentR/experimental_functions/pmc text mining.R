56874086375631bf7ee276afa8658b83297a284b
Sili Fan <silifan@SilideMacBook-Pro.local>
2019-08-11 23:07:42 -0700

    S

--- MetDADevelopmentR/experimental_functions/pmc text mining.R
+++ MetDADevelopmentR/experimental_functions/pmc text mining.R
@@ -1,8 +1,18 @@
+# Convenience function to convert html codes
+html2txt <- function(str) {
+  xpathApply(htmlParse(str, asText=TRUE),
+             "//body//text()",
+             xmlValue)[[1]]
+}
+
 artical_limitation = 30
 
-pacman::p_load(jsonlite, xml2, httr, XML)
-interested_term = "t-test"
-interested_term_url = GET(URLencode(paste0('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&term=',interested_term)))$content
+pacman::p_load(jsonlite, xml2, httr, XML, data.table, textreadr, gsubfn)
+
+
+
+interested_term = " metabolomics "
+interested_term_url = GET(URLencode(paste0('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&term="',interested_term,'"&retmax=100000')))$content
 interested_term_obj = read_xml(interested_term_url)
 # interested_term_text = xml_text(, trim= TRUE)
 
@@ -10,62 +20,116 @@ interested_term_list = as_list(interested_term_obj)
 
 interested_term_id_list = unlist(interested_term_list $eSearchResult$IdList)
 
-interested_term_id_list = interested_term_id_list[1:artical_limitation]
+# interested_term_id_list = interested_term_id_list[1:artical_limitation]
+
+
+# interested_term_id_list_power = interested_term_id_list
+interested_term_id_list_metabolomics = interested_term_id_list
+# interested_term_id_list_FDR = interested_term_id_list
+
+intersect(intersect(interested_term_id_list_power, interested_term_id_list_metabolomics), interested_term_id_list_FDR)
+
+
+'5974217' %in% interested_term_id_list_power
+
+
+
+
+
+
+
 
 # now we've found the id list. Now, let's go on to extract the text.
-
-# !!!! As mentioned in our Open Access Subset page, the majority of articles in PMC are subject to traditional copyright restrictions, and are not available for downloading in bulk.
 interested_term_string = gsub("[^a-zA-Z]", " ", interested_term)
-
 paper_texts = list()
+interested_term_id_list = interested_term_id_list[!is.na(interested_term_id_list)]
 for(i in 1:length(interested_term_id_list)){
   current_id = interested_term_id_list[i]
-  paper_url <- rawToChar(GET(paste0("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pmc&id=",current_id))$content)
-  paper_obj = read_xml(paper_url)
-  paper_text = xml_text(paper_obj, trim= TRUE)
-  paper_split = strsplit(paper_text," \\|")
 
+  # current_id='6687334'
 
-  indexes = unname(sapply(paper_split[[1]], function(x){
-    grepl(interested_term_string,gsub("[^a-zA-Z]", " ", x))
+  paper_html = readLines(paste0("https://www.ncbi.nlm.nih.gov/pmc/articles/PMC",current_id))
+  paper_html = paste0(paper_html,collapse = "")
+  writeLines(substr(paper_html, 10, nchar(paper_html)),"paper_text.txt")
+  paper_split_on_div = strsplit(paper_html, '<div id="')[[1]]
+  paper_split_on_div_index = unname(sapply(paper_split_on_div, function(x){
+    grepl(toupper(interested_term_string),gsub("[^a-zA-Z]", " ", toupper(gsub("<.*?>", "", x))))
   }))
 
-  paper_texts[[current_id]] = paper_split[[1]][indexes]
+  targeted_text = paper_split_on_div[paper_split_on_div_index]
+
+  targeted_text = sapply(targeted_text, function(x){
+    strsplit(x, "<p")[[1]]
+  })
+
+  targeted_text = unlist(targeted_text)
+
+  targeted_text_index = unname(sapply(targeted_text, function(x){
+    grepl(toupper(interested_term_string),gsub("[^a-zA-Z]", " ", toupper(gsub("<.*?>", "", x))))
+  }))
+
+  if(length(targeted_text_index)>0){
+    targeted_text = unname(sapply(paste0("<p",targeted_text[targeted_text_index]), function(x){
+      html2txt(gsub("<.*?>", "", x))
+    }))
+
+
+    paper_texts[[current_id]] = targeted_text
+  }
+
+
+
+  # paper_texts[[current_id]]
+  # i = i + 1
+  # text started with "(" should be dissmised.
 
 
 }
 
-test = htmlToText("https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6686905/")
+pacman::p_load(officer, magrittr)
+doc <- read_docx()
+for(i in 1:length(paper_texts)){
+   doc = doc %>%
+    body_add_par(value = names(paper_texts)[i], style = "heading 1")
+
+   for(j in 1:length(paper_texts[[i]])){
+
+     doc = doc %>%
+       body_add_par(paper_texts[[i]][j], style = "Normal")
+
+
+   }
+
+
+}
+
+
+
+print(doc, target = paste0("docs/",interested_term,".docx"))
 
 
 
 
 
-xml.url = rawToChar(GET(xml.url)$content)
-xmlfile <- xmlTreeParse(xml.url)
-
-
-xmltop = xmlRoot(xmlfile)
-
-character = as.character(xmltop)
-
-
-
-plantcat <- xmlSApply(xmltop, function(x) xmlSApply(x, xmlValue))
-
-
-plantcat_df <- data.frame(t(plantcat),row.names=NULL)
-
-
-
-xmllist = xmlToList(xml.url)
-
-
-xmljson = toJSON(xmllist)
 
 
 
 
-xmltext = xml_text(read_xml(xml.url), trim= TRUE)
 
-tt = strsplit(xmltext," \\|")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MetDADevelopmentR/experimental_functions/power analyses.R
@@ -0,0 +1,188 @@
+# power analysis
+
+# 1 https://www.bioconductor.org/packages/release/bioc/manuals/ssize/man/ssize.pdf
+pacman::p_load(ssize)
+library(gdata) # for nobs()
+data(exp.sd)
+# Histogram of the standard deviations
+hist(exp.sd,n=20, col="cyan", border="blue", main="",
+     xlab="Standard Deviation (for data on the log scale)")
+dens <- density(exp.sd)
+lines(dens$x, dens$y*par("usr")[4]/max(dens$y),col="red",lwd=2)
+title("Histogram of Standard Deviations")
+# 1) What is the power if using 6 patients 3 measurements assuming
+# Delta=1.0, Alpha=0.05 and Observed SDs?
+n=6; fold.change=2.0; power=0.8; sig.level=0.05;
+#
+all.power <- pow(sd=exp.sd, n=n, delta=log2(fold.change),
+                 sig.level=sig.level)
+power.plot(all.power, lwd=2, col="blue")
+xmax <- par("usr")[2]-0.05; ymax <- par("usr")[4]-0.05
+legend(x=xmax, y=ymax,
+       legend= strsplit( paste("n=",n,",",
+                               "fold change=",fold.change,",",
+                               "alpha=", sig.level, ",",
+                               "# genes=", nobs(exp.sd), sep=''), "," )[[1]],
+       xjust=1, yjust=1, cex=1.0)
+title("Power to Detect 2-Fold Change")
+
+
+# 2) What is necessary sample size for 80% power using 3 measurements/patient
+# assuming Delta=1.0, Alpha=0.05 and Observed SDs?
+#
+
+all.size <- ssize(sd=exp.sd, delta=log2(fold.change),
+                  sig.level=sig.level, power=power)
+ssize.plot(all.size, lwd=2, col="magenta", xlim=c(1,20))
+xmax <- par("usr")[2]-1; ymin <- par("usr")[3] + 0.05
+legend(x=xmax, y=ymin,
+       legend= strsplit( paste("fold change=",fold.change,",",
+                               "alpha=", sig.level, ",",
+                               "power=",power,",",
+                               "# genes=", nobs(exp.sd), sep=''), "," )[[1]],
+       xjust=1, yjust=0, cex=1.0)
+title("Sample Size to Detect 2-Fold Change")
+
+
+# 3) What is necessary fold change to achieve 80% power using 3
+# measurements/patient assuming n=6, Delta=1.0, Alpha=0.05 and Observed
+# SDs?
+#
+all.delta <- delta(sd=exp.sd, power=power, n=n,
+                   sig.level=sig.level)
+delta.plot(all.delta, lwd=2, col="magenta", xlim=c(1,10))
+xmax <- par("usr")[2]-1; ymin <- par("usr")[3] + 0.05
+legend(x=xmax, y=ymin,
+       legend= strsplit( paste("n=",n,",",
+                               "alpha=", sig.level, ",",
+                               "power=",power,",",
+                               "# genes=", nobs(exp.sd), sep=''), "," )[[1]],
+       xjust=1, yjust=0, cex=1.0)
+title("Fold Change to Achieve 80% Power")
+
+
+
+
+# 1 https://cran.r-project.org/web/packages/ssize.fdr/ssize.fdr.pdf
+pacman::p_load(ssize.fdr)
+# In words, (4.2) says the q-value is a measure of the strength of an observed
+# statistic with respect to the pFDR; it is the minimum pFDR that can occur when
+# rejecting a statistic with value t for the set of nested significance regions.
+
+# adjusted p value most stringent. q value middle. p value most loose.
+
+
+a<-0.05 ##false discovery rate to be controlled
+pwr<-0.8 ##desired power
+p0<-c(0.5,0.9,0.95) ##proportions of non-differentially expressed genes
+N<-20; N1<-35 ##maximum sample size for calculations
+
+##Example of function ssize.oneSamp
+d<-1 ##effect size
+s<-0.5 ##standard deviation
+os<-ssize.oneSamp(delta=d,sigma=s,fdr=a,power=pwr,pi0=p0,maxN=N,side="two-sided")
+os$ssize ##first sample sizes to reach desired power
+os$power ##calculated power for each sample size
+os$crit.vals ##calculated critical value for each sample size
+
+
+##Example of function ssize.oneSampVary
+dm<-2; ds<-1 ##the effect sizes of the genes follow a Normal(2,1) distribution
+alph<-3; beta<-1 ##the variances of the genes follow an Inverse Gamma(3,1) distribution.
+osv<-ssize.oneSampVary(deltaMean=dm,deltaSE=ds,a=alph,b=beta,fdr=a,power=pwr,pi0=p0,
+                       maxN=N1,side="two-sided")
+osv$ssize ##first sample sizes to reach desired power
+osv$power ##calculated power for each sample size
+osv$crit.vals ##calculated critical value for each sample size
+
+
+
+##Example of function ssize.twoSamp
+##Calculates sample sizes for two-sample microarray experiments
+##See Figure 1.(a) of Liu & Hwang (2007)
+d1<-1 ##difference in differentially expressed genes to be detected
+s1<-0.5 ##standard deviation
+ts<-ssize.twoSamp(delta=d1,sigma=s1,fdr=a,power=pwr,pi0=p0,maxN=N,side="two-sided")
+ts$ssize ##first sample sizes to reach desired power
+ts$power ##calculated power for each sample size
+ts$crit.vals ##calculated critical value for each sample size
+
+
+
+##Example of function ssize.F
+##Sample size calculation for three-treatment loop design microarray experiment
+##See Figure S2. of Liu & Hwang (2007)
+des<-matrix(c(1,-1,0,0,1,-1),ncol=2,byrow=FALSE) ##design matrix of loop design experiment
+b<-c(1,-0.5) ##difference between first two treatments is 1 and second and third
+##treatments is -0.5
+df<-function(n){3*n-2} ##degrees of freedom for this design is 3n-2
+s<-1 ##standard deviation
+p0.F<-c(0.5,0.9,0.95,0.995) ##proportions of non-differentially expressed genes
+ft<-ssize.F(X=des,beta=b,dn=df,sigma=s,fdr=a,power=pwr,pi0=p0.F,maxN=N)
+ft$ssize ##first sample sizes to reach desired power
+ft$power ##calculated power for each sample size
+ft$crit.vals ##calculated critical value for each sample sizeft$ssize
+
+
+# 5737945
+
+
+# MetSizeR
+
+ssize = 10
+
+data = matrix(rnorm(1000*ssize*2), nrow = 1000, ncol = ssize*2)
+group = rep(c("A","B"),ssize)
+
+
+data[1:50, group == "B"] = data[1:50, group == "B"]+1
+
+ts = list()
+m = 0.05
+FDR=c()
+for(t in 1:20){
+
+   ts[[t]] = apply(data[sample(1:nrow(data)),], 1, function(x){
+           t.test(x ~ group)$statistic
+   })
+   p0 = m * nrow(data)
+   sample_index = sample(1:nrow(data), p0)
+
+
+   y = as.numeric(factor(group))
+   x = t(data)
+   n11 = ssize-1
+   n22 = ssize-1
+   in1n2 = 1/ssize+1/ssize
+   nn2 = ssize + ssize - 2
+   cpcf = p0
+
+           yperm<-sample(y, replace=FALSE)
+           x1<-x[yperm==1,]
+           x2<-x[yperm==2,]
+           Sj<-sqrt(in1n2*(n11*diag(var(x1))+n22*diag(var(x2)))/nn2)  # FASTER!!!
+
+
+
+   ts[[t]][sample_index] = ts[[t]][sample_index]+1/Sj[sample_index]
+
+   p_th = sort(abs(ts[[t]]), decreasing = TRUE)[p0]
+
+   FDR[t] = sum(abs(ts[[t]][c(1:nrow(data))[!c(1:nrow(data)) %in% sample_index]])>p_th)/sum(abs(ts[[t]]) > p_th)
+
+
+
+}
+mean(FDR)
+
+p_vals = apply(data, 1, function(x){
+        t.test(x ~ group)$p.value
+})
+
+sum(p_vals[51:nrow(data)]<0.05)/sum(p_vals<0.05)
+
+
+
+
+
+
