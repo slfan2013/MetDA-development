0cd6d201d644f61de3e491f513d6e4d07330b729
Sili Fan <fansili2013@gmail.com>
2019-08-13 21:24:26 -0700

    S

--- js/sample_information_non_changing_levels_select.js
+++ js/sample_information_non_changing_levels_select.js
@@ -2,7 +2,7 @@ p_column_names = Object.keys(p[0])
 
 //console.log(id)
 
-sample_information_non_changing_levels_select_div = '<div class="form-group"><label>Treatment Groups</label><select class="form-control selectpicker parameter" id="'+id+'" data-style="btn btn-link">'
+sample_information_non_changing_levels_select_div = '<div class="form-group"><select class="form-control selectpicker parameter" id="'+id+'" data-style="btn btn-link">'
 for (var i = 0; i < p_column_names.length; i++) {
     if (p_column_names[i] !== 'label') {
         sample_information_non_changing_levels_select_div = sample_information_non_changing_levels_select_div + '<option>' + p_column_names[i] + '</option>'
@@ -10,3 +10,5 @@ for (var i = 0; i < p_column_names.length; i++) {
 }
 sample_information_non_changing_levels_select_div = sample_information_non_changing_levels_select_div + '</select></div>'
 $("#"+div_id).html(sample_information_non_changing_levels_select_div)
+
+
--- js/ssize_parameter_settings.js
+++ js/ssize_parameter_settings.js
@@ -3,3 +3,21 @@ console.log("ssize_parameter_settings.js")
 div_id = "sample_information_non_changing_levels_select_div"
 id = "treatment_group"
 $("#"+div_id).load("sample_information_non_changing_levels_select.html", init_selectpicker)
+
+
+setTimeout(function(){
+    div_id = "sample_information_non_changing_levels_select_div_sample_id"
+    id = "sample_id"
+    $("#"+div_id).load("sample_information_non_changing_levels_select.html", init_selectpicker)
+}, 100)
+
+
+$("#test_type").change(function(){
+    if(['paired t-test','repeated ANOVA'].includes($("#test_type").val())){
+        $("#sample_information_non_changing_levels_select_div_sample_id").show()
+    }else{
+        $("#sample_information_non_changing_levels_select_div_sample_id").hide()
+    }
+})
+$("#sample_information_non_changing_levels_select_div_sample_id").hide()
+
